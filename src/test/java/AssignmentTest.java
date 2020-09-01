4ddec64303b0bc1b590d81457613e9c31b7a1844
ParthoShuvo <shuvojitmail.gmail.com>
2020-09-02 00:18:04 +0600

    initial project structure

---
+++ src/test/java/AssignmentTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class AssignmentTest {
+
+
+    @Test
+    void justTest() {
+        assertEquals("This is just a test", "This is just a test");
+    }
+}
