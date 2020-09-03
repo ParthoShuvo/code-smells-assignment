4455a66604d5cb029dcbbb5dac8d7146782cc55e
Shuvojit Saha <shuvojit.saha@kaz.com.bd>
2020-09-03 15:04:21 +0600

    Refactoring Extract to method

--- src/main/java/applicationlevel/refactoring/DistanceCalculator.java
+++ src/main/java/applicationlevel/refactoring/DistanceCalculator.java
@@ -2,7 +2,11 @@ package applicationlevel.refactoring;
 
 public class DistanceCalculator {
     public double betweenPoints(Point p1, Point p2) {
-        return Math.sqrt(Math.pow((p2.getY() - p1.getY()), 2) + Math.pow(p2.getX() - p1.getX(), 2));
+        return Math.sqrt(square(p2.getY() - p1.getY()) + square(p2.getX() - p1.getX()));
+    }
+
+    private double square(int val) {
+        return val * val;
     }
 
     public double toOriginFrom(Point p) {
